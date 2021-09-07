45ce85ffb2b6c44757e9b3db719704d2fdbedeb3
lahincapie <alejandra612@hotmail.com>
2021-09-07 16:28:21 -0500

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,4 +4,4 @@ for numL in range(0, 10):
         if numL == 8 and numR == 9:
             print("{:d}{:d}".format(numL, numR))
         else:
-            print("{:d}{:d},".format(numL, numR), end='')
+            print("{:d}{:d}, ".format(numL, numR), end='')
