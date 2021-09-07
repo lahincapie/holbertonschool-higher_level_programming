8d26de82ad8a231b491843bf7daa5c412294202a
lahincapie <alejandra612@hotmail.com>
2021-09-07 16:26:20 -0500

    6-print_comb3.py

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for numL in range(0, 10):
+    for numR in range(numL + 1, 10):
+        if numL == 8 and numR == 9:
+            print("{:d}{:d}".format(numL, numR))
+        else:
+            print("{:d}{:d},".format(numL, numR), end='')
