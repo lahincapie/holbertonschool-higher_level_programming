055000cd3950aa9295ef7b3e84c8e461e681d6c7
lahincapie <alejandra612@hotmail.com>
2021-09-08 17:55:45 -0500

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     if argc == 0:
         print("0 arguments.")
     elif argc == 1:
-        print("1 argument: ")
+        print("1 argument:")
         print("1: {}".format(sys.argv[1]))
     else:
         print("{:d} arguments: ".format(argc))
