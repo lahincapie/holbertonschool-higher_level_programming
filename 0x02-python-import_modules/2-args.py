996fe9f1382fdc01f5f710ddc938ced82c9c8773
lahincapie <alejandra612@hotmail.com>
2021-09-08 17:50:05 -0500

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
         print("1 argument: ")
         print("1: {}".format(sys.argv[1]))
     else:
-        print("{} arguments: ".format(argc))
+        print("{:d} arguments: ".format(argc))
         for count in range(1, argc + 1):
             print("{}: {}".format(count, sys.argv[count]))
