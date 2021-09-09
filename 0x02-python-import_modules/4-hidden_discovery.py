66b6badb048cbba88ebbc18571b36ff8b5ac93bf
lahincapie <alejandra612@hotmail.com>
2021-09-08 19:35:05 -0500

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
+import hidden_4
 if __name__ == "__main__":
-    import hidden_4
-    hidd_str = dir(hidden_4)
-    for i in hidd_str:
-        if i[0] == "_":
-            continue
-        print("{}".format(i))
+    a = dir(hidden_4)
+    for i in range(len(a)):
+        if a[i][:2] != "__":
+            print(a[i])
