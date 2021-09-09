076e55470eb32dacb9f032ada43ced6312eedc98
lahincapie <alejandra612@hotmail.com>
2021-09-08 19:28:42 -0500

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+import hidden_4
+
+
+def discovr():
+    name = dir(hidden_4)
+    for i in name:
+        if i[:2] != '__':
+            print("{:s}".format(i))
+
+
 if __name__ == "__main__":
-    import hidden_4
-    for name in sorted(dir(hidden_4)):
-        if name[:2] != '__':
-            print("{}".format(name))
+    discovr()
