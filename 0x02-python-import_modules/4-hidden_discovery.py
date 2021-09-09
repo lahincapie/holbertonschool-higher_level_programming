17c340c886a4913fe1c6770d9f4d47acf8563733
lahincapie <alejandra612@hotmail.com>
2021-09-08 19:14:33 -0500

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,4 +3,4 @@ if _name_ == "_main_":
     import hidden_4
     for name in dir(hidden_4):
         if name[:2] != "__":
-            print("{:name}".format(name))
+            print("{}".format(name))
