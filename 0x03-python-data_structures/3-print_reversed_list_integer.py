3dca161b39aed810c8539bd6f7ee9ec3c5857201
lahincapie <alejandra612@hotmail.com>
2021-09-13 14:57:51 -0500

    3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if my_list is None:
+        return None
     """ [inicio : fin : pasos]  """
     """ [inicia en 0 : hasta el final : de uno en uno desde el final ]  """
     for item in my_list[::-1]:
