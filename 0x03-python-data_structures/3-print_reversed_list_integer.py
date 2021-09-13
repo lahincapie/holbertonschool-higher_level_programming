5f8c0ed4bfd32ed57b80bd21ba7aac1808622f6c
lahincapie <alejandra612@hotmail.com>
2021-09-13 14:51:31 -0500

    3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,4 +3,4 @@ def print_reversed_list_integer(my_list=[]):
     """ [inicio : fin : pasos]  """
     """ [inicia en 0 : hasta el final : de uno en uno desde el final ]  """
     for item in my_list[::-1]:
-        print(item)
+        print("{:d}".format(item))
