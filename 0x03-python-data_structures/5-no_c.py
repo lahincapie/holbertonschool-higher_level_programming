4699a38f37b48d7082872827775c5bf977c4756e
lahincapie <alejandra612@hotmail.com>
2021-09-13 16:12:36 -0500

    5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    my_new_string = ""
+    letter = 0
+    while letter < len(my_string):
+        if my_string[letter] != "C" and my_string[letter] != "c":
+            my_new_string += my_string[letter]
+        letter += 1
+    return my_new_string
