ee68bcd71d355b61611696d5663970c88cc0e712
lahincapie <alejandra612@hotmail.com>
2021-09-13 20:17:08 -0500

    9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        max = my_list[0]
+        for aux in my_list:
+            if aux > max:
+                max = i
+        return max
+    return (None)
