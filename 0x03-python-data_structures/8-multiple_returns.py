be8335768de77e75b149ff846d32991f130a93b1
lahincapie <alejandra612@hotmail.com>
2021-09-13 19:48:40 -0500

    8-multiple_returns.py

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    len_sentence = len(sentence)
+    if len_sentence == 0:
+        return (len_sentence, None)
+    return (len_sentence, sentence[0])
