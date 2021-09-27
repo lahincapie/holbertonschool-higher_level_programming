7a788b18bd0bd275de84fbf06ee0c44a6f59fac9
lahincapie <alejandra612@hotmail.com>
2021-09-27 14:17:00 -0500

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
         return x
     try:
         for i in range(0, x):
-            print(my_list[i], end="")
+            print("{:d}".format(my_list[i]), end="")
     except:
         print("")
         return i
