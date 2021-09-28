9276eb84293c2f18ca85ddff1e1dadf45baeebe7
lahincapie <alejandra612@hotmail.com>
2021-09-27 19:26:16 -0500

    3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,6 +5,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         division = None
     finally:
-        print("Inside result: {}" .format(division))
+        print("Inside result: {}".format(division))
     return division
-
