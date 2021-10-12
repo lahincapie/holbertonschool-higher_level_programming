70bc58dfc080dcd325d6c80b349e52588d04169e
lahincapie <alejandra612@hotmail.com>
2021-10-12 17:51:57 -0500

    0-read_file.py README.md

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''function that reads a text file (UTF8) and prints it to stdout'''
+
+
+def read_file(filename=""):
+    '''Using with is also much shorter than writing
+    equivalent try-finally blocks:'''
+    with open(filename, mode="r", encoding="utf-8") as f:
+        my_file = f.read()
+        print(my_file)
