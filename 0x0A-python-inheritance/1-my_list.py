36b7207602da746f67e99c9f41fa9b981395dc57
lahincapie <alejandra612@hotmail.com>
2021-10-11 19:00:29 -0500

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,7 +5,7 @@ this function contains the MyList class
 class MyList(list):
 
     def __init__(self):
-        pass
+        super().__init__()
 
     '''Public instance method of class MyList'''
     def print_sorted(self):
