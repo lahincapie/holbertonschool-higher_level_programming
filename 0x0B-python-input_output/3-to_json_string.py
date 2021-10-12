7b2883e32d6859f0b58c310e0a630cde18b51e8f
lahincapie <alejandra612@hotmail.com>
2021-10-12 18:21:39 -0500

    3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -5,4 +5,8 @@ of an object (string)'''
 
 
 def to_json_string(my_obj):
+    '''Serialize obj to a JSON formatted str.
+    If skipkeys is true then dict keys that are
+    not basic types (str, int, float, bool, None)
+    will be skipped instead of raising a TypeError.'''
     return json.dumps(my_obj)
