127b61e906b3b3bfd76b5716f1e60e8d9cd377e0
lahincapie <alejandra612@hotmail.com>
2021-10-12 18:29:50 -0500

    3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-'''function that returns the JSON representation
-of an object (string)'''
+'''
+function that returns the JSON representation
+of an object (string)
+'''
 import json
 
 
