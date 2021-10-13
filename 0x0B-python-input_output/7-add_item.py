76fa9d35b5ffb49b528f6cfc4758db6a0ad69398
lahincapie <alejandra612@hotmail.com>
2021-10-12 21:04:38 -0500

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,15 +5,16 @@ import sys
 import os
 
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-f = "add_item.json"
-my_list = []
-if os.path.isfile(f):
-    my_list = load_from_json_file(f)
+    f = "add_item.json"
+    my_list = []
+    if os.path.isfile(f):
+        my_list = load_from_json_file(f)
 
-for args in sys.argv:
-    my_list.append(args)
+    for args in sys.argv:
+        my_list.append(args)
 
-save_to_json_file(my_list, f)
+    save_to_json_file(my_list, f)
