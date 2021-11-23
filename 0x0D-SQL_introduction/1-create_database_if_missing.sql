3335543a990e4852c9b2f1ee9bc07f4234ecad5c
lahincapie <alejandra612@hotmail.com>
2021-11-23 14:30:46 -0500

    1-create_database_if_missing.sql

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,4 @@
---script that creates the database hbtn_0c_0 in your MySQL server.
+--script that creates the database
+-- hbtn_0c_0 in your MySQL server.
+
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
