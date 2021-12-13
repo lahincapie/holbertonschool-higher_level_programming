96a104a1081c7711050513b9b7ab28e81f1f1754
lahincapie <alejandra612@hotmail.com>
2021-12-13 16:50:48 -0500

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-import { argv } from 'process';
-if (argv.length <= 2) {
+const argvLength = process.argv.length;
+if (argvLength === 2) {
   console.log('No argument');
-} else if (argv.length === 3) {
+} else if (argvLength === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
