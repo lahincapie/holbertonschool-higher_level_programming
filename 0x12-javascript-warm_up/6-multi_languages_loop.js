10e8c307aad5f25ce2cc84885a1985e161161f32
lahincapie <alejandra612@hotmail.com>
2021-12-13 19:27:02 -0500

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const msg = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let index = 0;
-while (index < msg.length - 1) {
+while (index <= msg.length - 1) {
   console.log(msg[index]);
   index = index + 1;
 }
