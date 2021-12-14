23a83573acf4c6aae6413ccd60707796fffffc92
lahincapie <alejandra612@hotmail.com>
2021-12-13 19:50:21 -0500

    7-multi_c.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const myNumber = parseInt(process.argv[2]);
+let index = 0;
+if (Number.isNaN(myNumber)) {
+    console.log('Missing number of occurrences');
+} else {
+    while (index < myNumber) {
+        console.log('C is fun');
+        index = index + 1;
+    }
+}
