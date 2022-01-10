1d896e7e36b605a6734bb08cd87d240ac722b589
lahincapie <alejandra612@hotmail.com>
2022-01-09 19:23:08 -0500

    0-body_size.sh

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Bash script that takes in a URL,
+# sends a request to that URL,
+# and displays the size of the body
+# of the response
+
+curl "$1" -sI |grep "Content-Length"| cut -d ':' -f 2 | cut -d ' ' -f 2
