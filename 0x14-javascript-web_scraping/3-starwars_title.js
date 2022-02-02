d51d48c0dfde878cf2fe66655648bbe0ebd90318
lahincapie <alejandra612@hotmail.com>
2022-02-01 22:20:25 -0500

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
-let url = 'http://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
+
+const url = 'https://swapi-api.hbtn.io/api/films/';
+request(url + process.argv[2], function (error, response, body) {
   console.log(error || JSON.parse(body).title);
 });
